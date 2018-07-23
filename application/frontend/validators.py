050e36a9a4a26993276f1697ef75aebb5e1c9d35
Adam Shimali <adam.shimali@gmail.com>
2018-07-23 14:55:12 +0100

    merge from master.
    Do some post processing of validation results to make report to
    front end a bit easier.

--- application/frontend/validators.py
+++ application/frontend/validators.py
@@ -1,102 +1,235 @@
-from vladiate import Vlad
-from vladiate.exceptions import ValidationException
-from vladiate.validators import Validator, SetValidator, FloatValidator, IntValidator, Ignore, RegexValidator, \
-    NotEmptyValidator
+import csv
+
+from io import StringIO
+from collections import defaultdict
+
+import requests
+
+from enum import Enum
+
+from requests.exceptions import InvalidSchema, HTTPError, MissingSchema
 
 
-class ValidatorWarning:
+class Error(Enum):
+    INVALID_URL = 'An invalid URL was found in the file'
+    INVALID_DATE = 'Date format should be YYYY-MM-DD'
+    NO_LINE_BREAK = 'No line breaks allowed'
+    REQUIRED_FIELD = 'Required data is missing'
+    INVALID_FLOAT = 'This field must contain a floating point number'
 
-    def __init__(self, name, message):
-        self.name = name
+
+class Warning(Enum):
+    HTTP_WARNING = 'There was a problem fetching data from a URL in the file'
+    DATE_WARNING = 'The date is in the future. Please check'
+    CONTENT_TYPE_WARNING = 'Set Content-Type to test/csv;charset-utf8'
+    FILE_ENCODING_WARNING = 'File should be encoded as utf-8'
+
+
+class StringInput():
+
+    ''' Read a file from a string '''
+
+    def __init__(self, string_input=None, string_io=None):
+        self.string_io = string_io if string_io else StringIO(string_input)
+
+    def open(self):
+        return self.string_io
+
+
+class ValidationIssue:
+
+    def __init__(self, data, type_, message = None):
+        self.data = data
+        self.type_ = type_
         self.message = message
 
 
-class URLValidator(Validator):
+class BaseFieldValidator:
+
+    def __init__(self, allow_empty=False):
+        self.allow_empty = allow_empty
+
+    def validate(self, field, row):
+        raise NotImplementedError
+
+
+class URLValidator(BaseFieldValidator):
+
+    ''' Field must be a valid url and reachable '''
 
     def __init__(self, **kwargs):
         super(URLValidator, self).__init__(**kwargs)
-        self.failures = set([])
+        self.checked = set([])
 
-    def validate(self, field, row={}):
-        if not self.empty_ok:
-            valid, exception = self.validate_url(field)
-            if not valid and (field or not self.empty_ok):
-                self.failures.add(field)
-                raise ValidationException("{} - {}".format(field, exception))
+    def validate(self, data, row={}):
+        errors = []
+        warnings = []
+        if (data or not self.allow_empty) and data not in self.checked:
+            try:
+                resp = requests.get(data)
+                resp.raise_for_status()
+                self.checked.add(data)
+            except (InvalidSchema, MissingSchema) as e:
+                errors.append(ValidationIssue(data, Error.INVALID_URL, message=e))
+            except HTTPError as e:
+                warnings.append(ValidationIssue(data, Warning.HTTP_WARNING, message=e))
 
-    @property
-    def bad(self):
-        return self.failures
-
-    @staticmethod
-    def validate_url(url):
-        from urllib.request import urlopen
-        from urllib.error import URLError, HTTPError
-        try:
-            urlopen(url)
-            return True, None
-        except HTTPError as e:
-            return False, e
-        except URLError as e:
-            return False, e.reason
-        except Exception as e:
-            return False, e
+        return errors, warnings
 
 
-class StringNoLineBreaksValidator(Validator):
+class StringNoLineBreaksValidator(BaseFieldValidator):
+
+    ''' Field cannot contain line breaks '''
 
     def __init__(self, **kwargs):
         super(StringNoLineBreaksValidator, self).__init__(**kwargs)
-        self.failures = set([])
 
-    def validate(self, field, row={}):
-        if not self.empty_ok:
-            valid = self.has_no_linebreaks(field)
-            if not valid and (field or not self.empty_ok):
-                self.failures.add(field)
-                raise ValidationException("{} contains line breaks".format(field))
-
-    @property
-    def bad(self):
-        return self.failures
-
-    @staticmethod
-    def has_no_linebreaks(field):
-        if '\r\n' in field or '\n' in field:
-            return False
-        return True
+    def validate(self, data, row={}):
+        errors = []
+        warnings = []
+        if (data or not self.allow_empty) and ('\r\n' in data or '\n' in data):
+            errors.append(ValidationIssue(data, Error.NO_LINE_BREAK))
+        return errors, warnings
 
 
-class ISO8601DateValidator(Validator):
+class ISO8601DateValidator(BaseFieldValidator):
+
+    ''' Field must be iso-8601 formatted date string '''
 
     def __init__(self, **kwargs):
         super(ISO8601DateValidator, self).__init__(**kwargs)
-        self.failures = set([])
+
+    def validate(self, data, row={}):
+        import datetime
+        errors = []
+        warnings = []
+        if data or not self.allow_empty:
+            try:
+                datetime.datetime.strptime(data, '%Y-%m-%d')
+            except ValueError as e:
+                errors.append(ValidationIssue(data, Error.INVALID_DATE))
+
+        return errors, warnings
+
+
+class NotEmptyValidator(BaseFieldValidator):
+
+    ''' Field cannot be empty '''
+
+    def __init__(self, **kwargs):
+        super(NotEmptyValidator, self).__init__(**kwargs)
+
+    def validate(self, data, row={}):
+        errors = []
+        warnings = []
+        if data.strip() == '':
+            errors.append(ValidationIssue(data, Error.REQUIRED_FIELD))
+        return errors, warnings
+
+
+class FloatValidator(BaseFieldValidator):
+
+    def __init__(self, **kwargs):
+        super(FloatValidator, self).__init__(**kwargs)
+
+    def validate(self, data, row={}):
+        errors = []
+        warnings = []
+        try:
+            if data or not self.allow_empty:
+                float(data)
+        except ValueError as e:
+            errors.append(ValidationIssue(data, Error.INVALID_FLOAT))
+        return errors, warnings
+
+
+class AnythingValidator(BaseFieldValidator):
+    ''' Field needs to be present but can be pretty much any content '''
 
     def validate(self, field, row={}):
-        if not self.empty_ok:
-            valid, message = self.validate_date(field)
-            if not valid and (field or not self.empty_ok):
-                self.failures.add(field)
-                raise ValidationException('{} {}'.format(field, message).strip())
-
-    @property
-    def bad(self):
-        return self.failures
-
-    @staticmethod
-    def validate_date(field):
-        if field.strip() == '':
-            return False, 'Date in format YYYY-MM-DD is required'
-        import datetime
-        try:
-            datetime.datetime.strptime(field, '%Y-%m-%d')
-            return True, None
-        except ValueError as e:
-            return False, 'Incorrect date format. Should be YYYY-MM-DD'
+        pass
 
 
-class BrownfieldSiteRegisterValidator(Vlad):
+class CrossFieldValidator(BaseFieldValidator):
+
+    ''' Checks field against another field in row '''
+
+    def __init__(self, validate_against_fields=[], **kwargs):
+        super(CrossFieldValidator, self).__init__(**kwargs)
+        self.validate_against_fields = validate_against_fields
+
+    def validate(self, field, row):
+        pass
+
+
+class RegisterValidator:
+
+    def __init__(self, source, file_warnings, line_count, validators={}, delimiter=None):
+
+        self.source = source
+        self.file_warnings = file_warnings
+        self.line_count = line_count
+        self.errors = defaultdict(lambda: defaultdict(list))
+        self.warnings = defaultdict(lambda: defaultdict(list))
+        self.missing = []
+        self.unknown = []
+        self.validators = validators or getattr(self, 'validators', {})
+        self.delimiter = delimiter or getattr(self, 'delimiter', ',')
+        self.rows_analysed = 0
+        self.report = {}
+
+    def validate(self):
+
+        reader = csv.DictReader(self.source.open(), delimiter=self.delimiter)
+        self.missing = set(self.validators) - set(reader.fieldnames)
+
+        for line, row in enumerate(reader):
+            self.rows_analysed += 1
+            for field_name, field in row.items():
+                if field_name in self.validators:
+                    for validator in self.validators[field_name]:
+                        errors, warnings = validator.validate(field, row=row)
+                        if errors:
+                            self.errors[field_name][line].extend(errors)
+                        if warnings:
+                            self.warnings[field_name][line].extend(warnings)
+                else:
+                    self.unknown.append(field_name)
+
+        self._gather_results()
+
+        return self
+
+    def _gather_results(self):
+
+        for field, errors in self.errors.items():
+            error_type_count = {}
+            for line_number, errs in errors.items():
+                for err in errs:
+                    if err.type_ not in error_type_count:
+                        error_type_count[err.type_] = 1
+                    else:
+                        error_type_count[err.type_] = error_type_count[err.type_] + 1
+            self.report[field] = {'errors' :  error_type_count}
+
+        for field, warnings in self.warnings.items():
+            warning_type_count = {}
+            for line_number, warns in warnings.items():
+                for warn in warns:
+                    if warn.type_ not in warning_type_count:
+                        warning_type_count[warn.type_] = 1
+                    else:
+                        warning_type_count[warn.type_] = warning_type_count[warn.type_] + 1
+            self.report[field] = {'warnings': warning_type_count}
+
+    def error_types_for_field(self, field):
+        return self.report[field]['errors']
+
+    def warning_types_for_field(self, field):
+        return self.report[field]['warnings']
+
+
+class BrownfieldSiteValidator(RegisterValidator):
 
     valid_coordinate_reference_system = ['WGS84', 'OSGB36', 'ETRS89']
     ownership_status_pattern = r'(?i)(owned by a public authority|not owned by a public authority|unknown ownership|mixed ownership)'
@@ -117,7 +250,7 @@ class BrownfieldSiteRegisterValidator(Vlad):
             StringNoLineBreaksValidator()
         ],
         'PreviouslyPartOf': [
-            StringNoLineBreaksValidator(empty_ok=True)
+            StringNoLineBreaksValidator(allow_empty=True)
         ],
         'SiteNameAddress': [
             NotEmptyValidator(),
@@ -129,7 +262,6 @@ class BrownfieldSiteRegisterValidator(Vlad):
         ],
         'CoordinateReferenceSystem': [
             NotEmptyValidator(),
-            SetValidator(valid_set=valid_coordinate_reference_system)
         ],
         'GeoX': [
             NotEmptyValidator(),
@@ -144,62 +276,37 @@ class BrownfieldSiteRegisterValidator(Vlad):
             FloatValidator()
         ],
         'OwnershipStatus': [
-            NotEmptyValidator(),
-            RegexValidator(pattern=ownership_status_pattern)
+            NotEmptyValidator()
         ],
-        'Deliverable': [
-            SetValidator(valid_set=['yes'], empty_ok=True)
-        ],
+        'Deliverable': [],
         'PlanningStatus': [
-            NotEmptyValidator(),
-            RegexValidator(pattern=planning_status_pattern)
+            NotEmptyValidator()
         ],
-        'PermissionType': [
-            RegexValidator(pattern=permission_type_pattern, empty_ok=True)
-        ],
+        'PermissionType': [],
         'PermissionDate': [
-            ISO8601DateValidator(empty_ok=True)
+            ISO8601DateValidator(allow_empty=True)
         ],
         'PlanningHistory': [
-            URLValidator(empty_ok=True)
+            URLValidator(allow_empty=True)
         ],
-        'ProposedForPIP': [
-            SetValidator(valid_set=['yes'], empty_ok=True)
-        ],
+        'ProposedForPIP': [],
         'MinNetDwellings': [
-            NotEmptyValidator(),
-            IntValidator()
+            NotEmptyValidator()
         ],
-        'DevelopmentDescription': [
-            Ignore()
-        ],
-        'NonHousingDevelopment': [
-            Ignore()
-        ],
-        'Part2': [
-            SetValidator(valid_set=['yes'], empty_ok=True)
-        ],
-        'NetDwellingsRangeFrom': [
-            IntValidator(empty_ok=True)
-        ],
-        'NetDwellingsRangeTo': [
-            IntValidator(empty_ok=True)
-        ],
-        'HazardousSubstances': [
-            SetValidator(valid_set=['yes'], empty_ok=True)
-        ],
+        'DevelopmentDescription': [],
+        'NonHousingDevelopment': [],
+        'Part2': [],
+        'NetDwellingsRangeFrom': [],
+        'NetDwellingsRangeTo': [],
+        'HazardousSubstances': [],
         'SiteInformation': [
-            URLValidator(empty_ok=True)
+            URLValidator(allow_empty=True)
         ],
-        'Notes': [
-            Ignore()
-        ],
+        'Notes': [],
         'FirstAddedDate': [
-            NotEmptyValidator(),
-            ISO8601DateValidator()
+            ISO8601DateValidator(),
         ],
         'LastUpdatedDate': [
-            NotEmptyValidator(),
             ISO8601DateValidator()
         ]
     }
--- application/frontend/views.py
+++ application/frontend/views.py
@@ -4,16 +4,11 @@ from bs4 import UnicodeDammit
 from flask import (
     Blueprint,
     render_template,
-    redirect,
-    url_for,
-    request,
-    abort
+    request
 )
 
-from vladiate.inputs import String
 from application.frontend.forms import BrownfieldSiteURLForm
-from application.frontend.validators import ValidatorWarning, BrownfieldSiteRegisterValidator
-
+from application.frontend.validators import BrownfieldSiteValidator, StringInput, Warning, ValidationIssue
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -26,60 +21,44 @@ def index():
 @frontend.route('/validate')
 def validate():
     form = BrownfieldSiteURLForm(request.args)
+
     if form.url.data and form.validate():
-        warnings, errors, missing, row_num = _get_data_and_validate(form.url.data)
-        if warnings or errors:
-            return render_template('fix.html', url=form.url.data, warnings=warnings, errors=errors, missing=missing, row_num=row_num)
+        result = _get_data_and_validate(form.url.data)
+        if result.file_warnings and result.errors:
+            return render_template('fix.html', url=form.url.data, result=result)
         else:
             from application.data.stubs import geojson
-            return render_template('valid.html', url=form.url.data, geojson=geojson)
+            return render_template('valid.html', url=form.url.data, geojson=geojson, result=result)
+
     return render_template('validate.html', form=form)
+
 
 @frontend.route('/error')
 def error():
-  return render_template('not-available.html')
+    return render_template('not-available.html')
+
 
 @frontend.context_processor
 def asset_path_context_processor():
     return {'asset_path': '/static/govuk_template'}
 
 
-# stub method for getting data and validating
 def _get_data_and_validate(url):
-    warnings = []
+
+    file_warnings = []
+
     resp = requests.get(url)
     content_type = resp.headers.get('Content-type')
     if content_type is not None and content_type != 'text/csv':
-        message = 'Expected text/csv, actual value %s' % content_type
-        warnings.append(ValidatorWarning('Content-Type', message=message))
+        file_warnings.append(ValidationIssue('Content-Type:%s' % content_type, Warning.CONTENT_TYPE_WARNING))
 
     dammit = UnicodeDammit(resp.content)
     encoding = dammit.original_encoding
     if encoding != 'utf-8':
-        message = 'Expected utf-8, actual value %s' % encoding
-        warnings.append(ValidatorWarning('File encoding', message=message))
+        file_warnings.append(ValidationIssue('File encoding: %s' % encoding, Warning.FILE_ENCODING_WARNING))
 
-    rows = resp.content.decode(encoding).splitlines()
-    validator = BrownfieldSiteRegisterValidator(source=String(string_input=resp.content.decode(encoding)),
-                                                ignore_missing_validators=True)
-    validator.validate()
+    content = resp.content.decode(encoding)
+    line_count = len(content.splitlines())
 
-    # unpack the validator error messages from the exception class until we come up with something tidy?
-    errors = []
-    for field, failure in validator.failures.items():
-        unpacked = []
-        for line_no, errs in failure.items():
-          obj = {
-            "line": line_no,
-            "errors": [message.args[0] for message in errs]
-          }
-          unpacked.append(obj)
-        errors.append({field: unpacked})
-
-    missing = []
-    for f in validator.missing_fields:
-        missing.append(f)
-
-    return warnings, errors, missing, len(rows)
-
-
+    validator = BrownfieldSiteValidator(source=StringInput(string_input=content), file_warnings=file_warnings, line_count=line_count)
+    return validator.validate()
