33c0a4e6cafda32f5e3288b77a9df7dfbea45ea3
Adam Shimali <adam.shimali@gmail.com>
2019-11-14 14:09:12 +0000

    Remove wtforms file extension check.

--- application/frontend/forms.py
+++ application/frontend/forms.py
@@ -5,6 +5,5 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 class UploadForm(FlaskForm):
 
     upload = FileField('file', validators=[
-        FileRequired(),
-        FileAllowed(['csv', 'xls', 'xlsx', 'xlsm'], "We couldn't process the file. Only csv or excel files allowed.")
+        FileRequired()
     ])
