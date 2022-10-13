fd0d2c3792aba9d06d4bad393ff8b829e0717611
Adam Shimali <adam.shimali@gmail.com>
2022-10-13 15:53:43 +0100

    fixup postgres config var

--- config/config.py
+++ config/config.py
@@ -11,6 +11,9 @@ class Config(object):
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     UPLOAD_FOLDER = '/tmp'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres"):
+        SQLALCHEMY_DATABASE_URI.replace("postgres", "postgresql")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
